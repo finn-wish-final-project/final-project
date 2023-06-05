f2745df2349e72a215609f84de9973669096b3bc
KimMinSeok <kimminseok333@gmail.com>
2023-06-05 21:03:29 +0900

    app.py 코드 수정본 토큰을 제외한 기능 보완 완료(postman을 이용한 API 테스트 진행중)

--- app.py
+++ app.py
@@ -7,18 +7,27 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 # 회원가입 API 엔드포인트
-@app.route('/singup', methods=['POST']) ## POST 방식으로 오는 입력만 받음
+@app.route('/signup', methods=['POST']) ## POST 방식으로 오는 입력만 받음
 def signup():
     # POST 요청으로부터 필요한 정보를 get_json으로 받아옴
     data = request.get_json() 
-    user_id = data['ID_EMAIL']
+    user_id = data['EMAIL']
     user_pw = data['PASSWORD']
-    user_birth = data['BIRTH']
+    user_birth = data['USER_BIRTH']
     user_name = data['USER_NAME']
-    user_phone = data['PHONE']
+    user_phone = data['PHONE_NUM']
    
     # 유효성 검사 
     if email_validation(user_id) and email_overlap(user_id) and pw_validation(user_pw) and name_validation(user_name) and birth_validation(user_birth) and phone_validation(user_phone):
+        connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', passwd='1807992102', charset='utf8')
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+        sql = "INSERT INTO USER_ID (EMAIL, PASSWORD, USER_BIRTH, USER_NAME, PHONE_NUM) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, (user_id, user_pw, user_birth, user_name, user_phone))
+        connection.commit()
+        
+        connection.close()
+
         return jsonify({'message': '회원가입이 완료되었습니다.'})
     else:
         return jsonify({'message': '양식에 맞게 다시 입력해 주세요.'})
@@ -30,64 +39,72 @@ def email_validation(email):
         return True
     else:
         return False
+    
 # 중복 이메일 주소 가입 방지    
 def email_overlap(email):
-    email = email
-    connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', pw='1807992102', charset='utf8')
+    connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', passwd='1807992102', charset='utf8')
     cursor = connection.cursor(pymysql.cursors.DictCursor)
-    sql = "SELECT * FROM USER_ID"
-    cursor.execute(sql, (email))
-    result = cursor.fetchall()
+
+    sql = "SELECT * FROM USER_ID WHERE EMAIL = %s"
+    cursor.execute(sql, (email,))
+    result = cursor.fetchone()
+
+    connection.close()
+
     if result:
-        return False, jsonify({'message': '이미 사용 중인 이메일 주소입니다.'})
+        return False 
+    # jsonify({'message': '이미 사용 중인 이메일 주소입니다.'})
     else:
         return True
+    
 # 최소 비밀번호 길이 확인
 def pw_validation(password):
-    password=password
     if len(password) >= 4:
         return True
     else: 
-        return False, jsonify({'message': '비밀번호를 4자 이상 입력해 주세요.'})
+        return False 
+    # jsonify({'message': '비밀번호를 4자 이상 입력해 주세요.'})
+
 # 유저 이름 입력 정보 확인
 def name_validation(name):
-    name=name
     if len(name) >= 1:
         return True 
     else:
-        return False, jsonify({'message':'이름을 1자리 이상 입력해 주세요.'})
+        return False 
+    # jsonify({'message':'이름을 1자리 이상 입력해 주세요.'})
+
 # 생일 6자리로 입력 확인
 def birth_validation(birth):
-    birth=birth
     if len(birth) == 6:
         return True 
     else:
-        return False, jsonify({'message':'생일을 6자리 숫자로 입력해 주세요.'})
+        return False 
+    # jsonify({'message':'생일을 6자리 숫자로 입력해 주세요.'})
+
 # 전화번호 양식 확인
 def phone_validation(phone):
-    phone=phone
     if 3 <= len(phone) <= 11:
         return True 
     else:
-        return False, jsonify({'message':'( - ) 기호 없이 전화번호만 입력해 주세요'})
-
+        return False 
+    # jsonify({'message':'( - ) 기호 없이 전화번호만 입력해 주세요'})
 
 # 로그인 API 엔드포인트
 @app.route('/login', methods=['POST'])
 def login():
-    connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', pw='1807992102', charset='utf8')
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-
     data = request.get_json()
-    user_id = data['ID_EMAIL']
+    user_id = data['EMAIL']
     user_pw = data['PASSWORD']
     user_name = data['USER_NAME']
 
-    sql = "SELECT * FROM USER_ID"
-    cursor.execute(sql, (user_id, user_pw))
+    connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', passwd='1807992102', charset='utf8')
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-    result = cursor.fetchall()
-    print (result)
+    sql = "SELECT * FROM USER_ID WHERE EMAIL = %s AND PASSWORD = %s"
+    cursor.execute(sql, (user_id, user_pw))
+    result = cursor.fetchone()
+
+    connection.close()
    
     # 사용자 정보를 데이터베이스에서 검증
     if result: 
