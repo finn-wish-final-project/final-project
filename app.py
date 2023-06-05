3303cd22dc5536052c3e68c8614a8462b98dc38f
KimMinSeok <kimminseok333@gmail.com>
2023-06-05 15:34:25 +0900

    app.py 수정본 - 유효성 검사 추가, 데이터베이스 연결/ 토큰 생성 및 세부적인 코드 수정 필요

--- app.py
+++ app.py
@@ -1,90 +1,106 @@
 from flask import Flask, request, jsonify, render_template
-from test import Hello
+import pymysql
+# from test import Hello
+
 
 app = Flask(__name__)
-
-# 가상의 데이터베이스를 대신해 사용할 users 딕셔너리 
-users = {}
+app.config['JSON_AS_ASCII'] = False
 
 # 회원가입 API 엔드포인트
 @app.route('/singup', methods=['POST']) ## POST 방식으로 오는 입력만 받음
 def signup():
     # POST 요청으로부터 필요한 정보를 get_json으로 받아옴
     data = request.get_json() 
-    user_id = data['USER_ID']
-    id_email = data['ID_EMAIL']
-    password = data['PASSWORD']
-    birth = data['BIRTH']
+    user_id = data['ID_EMAIL']
+    user_pw = data['PASSWORD']
+    user_birth = data['BIRTH']
     user_name = data['USER_NAME']
-    phone = data['PHONE']
+    user_phone = data['PHONE']
+   
+    # 유효성 검사 
+    if email_validation(user_id) and email_overlap(user_id) and pw_validation(user_pw) and name_validation(user_name) and birth_validation(user_birth) and phone_validation(user_phone):
+        return jsonify({'message': '회원가입이 완료되었습니다.'})
+    else:
+        return jsonify({'message': '양식에 맞게 다시 입력해 주세요.'})
 
-    # 사용자 정보를 데이터베이스에 저장
-    users[user_id] = id_email, password, birth, user_name, phone
-    
-    return jsonify({'message': '회원가입이 완료되었습니다.'})
+# 올바른 이메일 형식인지 확인
+def email_validation(email):
+    email = email
+    if '@' in email and  5 < len(email) < 30:
+        return True
+    else:
+        return False
+# 중복 이메일 주소 가입 방지    
+def email_overlap(email):
+    email = email
+    connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', pw='1807992102', charset='utf8')
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    sql = "SELECT * FROM USER_ID"
+    cursor.execute(sql, (email))
+    result = cursor.fetchall()
+    if result:
+        return False, jsonify({'message': '이미 사용 중인 이메일 주소입니다.'})
+    else:
+        return True
+# 최소 비밀번호 길이 확인
+def pw_validation(password):
+    password=password
+    if len(password) >= 4:
+        return True
+    else: 
+        return False, jsonify({'message': '비밀번호를 4자 이상 입력해 주세요.'})
+# 유저 이름 입력 정보 확인
+def name_validation(name):
+    name=name
+    if len(name) >= 1:
+        return True 
+    else:
+        return False, jsonify({'message':'이름을 1자리 이상 입력해 주세요.'})
+# 생일 6자리로 입력 확인
+def birth_validation(birth):
+    birth=birth
+    if len(birth) == 6:
+        return True 
+    else:
+        return False, jsonify({'message':'생일을 6자리 숫자로 입력해 주세요.'})
+# 전화번호 양식 확인
+def phone_validation(phone):
+    phone=phone
+    if 3 <= len(phone) <= 11:
+        return True 
+    else:
+        return False, jsonify({'message':'( - ) 기호 없이 전화번호만 입력해 주세요'})
+
 
 # 로그인 API 엔드포인트
 @app.route('/login', methods=['POST'])
 def login():
-    # POST 요청으로부터 필요한 정보를 받아옴
+    connection = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', pw='1807992102', charset='utf8')
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
     data = request.get_json()
-    user_id = data['USER_ID']
-    id_email = data['ID_EMAIL']
-    password = data['PASSWORD']
-    birth = data['BIRTH']
+    user_id = data['ID_EMAIL']
+    user_pw = data['PASSWORD']
     user_name = data['USER_NAME']
-    phone = data['PHONE']
 
+    sql = "SELECT * FROM USER_ID"
+    cursor.execute(sql, (user_id, user_pw))
+
+    result = cursor.fetchall()
+    print (result)
+   
     # 사용자 정보를 데이터베이스에서 검증
-    if user_id in users and users[user_id] == id_email and password and birth and user_name and phone:
-        # 로그인 성공 시 세션 또는 토큰 생성
-        session_token = generate_session_token() # 세션 토큰 생성 함수 호출
-
-        # 세션 또는 토큰을 클라이언트에 전달
-        return jsonify({'message': '로그인 성공', 'session_token': session_token})
+    if result: 
+        return jsonify({'message': f'{user_name}님 반갑습니다.'})
     else:
-        return jsonify({'message': '로그인 실패'})
+        return jsonify({'message': '아이디 또는 비밀번호가 잘못 입력되었습니다.'})
     
-def generate_session_token():
-    # 세션 또는 토큰 생성 로직을 구현하여 적절한 값을 반환
-    return 'example_token'
+if __name__ == "__main__":
+    app.run(host='localhost', port='5000', debug=True)
 
-if __name__ == '__main__':
-    app.run()
-### models.py
-# import pymysql
-# def users_info():
-#     conn = pymysql.connect(host='localhost', port=3306, db='finnwish', user='root', pw='', charset='utf8')
-#     curdic = conn.cursor(pymysql.cursors.DictCursor)
-#     sql = "SELECT * FROM USER_ID"
-#     curdic.execute(sql)
-#     users_data = curdic.fetchall()
-#     return users_data 
 
-### app.py
-# from flask import Flask, jsonify
-# from models import users_info
-
-# @app.route('/test')
-# def test():
-#     data = users_info()
-#     return jsonify(data)
-
-### runserver.py
-# from flask import Flask
-# from app import app
-
-# if __name__ == "__main__":
-#     app.run(host='localhost', port='5000', debug=True)
-
-## python runserver.py
-## 웹에서 json 형태로 데이터 잘 출력되는지 확인
-
-## app 선언 아래에 아래 코드를 설정하여 jsonify 한글 인코딩 변경으로 인한 비정상 출력 문제 해결
-## app.config['JSON_AS_ASCII'] = False
-    
-def hello_flask():
-    return Hello
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000) 
+# 코드를 작성한 파일을 실행하거나 flask run 명령을 사용하여 Flask 애플리케이션을 실행합니다.
+# Postman, cURL 또는 웹 브라우저와 같은 도구를 사용하여 회원가입과 로그인 API 엔드포인트를 호출합니다.
+# 회원가입에는 POST 요청을 /signup 엔드포인트로 보내고 필요한 데이터를 JSON 형식으로 전송합니다.
+# 로그인에는 POST 요청을 /login 엔드포인트로 보내고 필요한 데이터를 JSON 형식으로 전송합니다.
+# 각 API 엔드포인트는 요청을 받아 처리하고 응답으로 JSON 데이터를 반환합니다. 응답을 확인하여 예상된 메시지가 올바르게 반환되는지 확인할 수 있습니다.
