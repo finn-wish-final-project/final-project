21bd97a5c17e7c9c0c6367042e235e4934e54473
chanseop <dlackstjq23@gmail.com>
2023-06-14 22:57:55 +0900

    게시판 api 및 챌린지 추천

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import JWTManager,create_access_token,jwt_required,get_j
 import pymysql
 from dotenv import load_dotenv
 from datetime import timedelta
+import numpy as np
+from sklearn.metrics import mean_squared_error
 import os 
 import ast
 
@@ -26,10 +28,181 @@ bcrypt=Bcrypt(app)
 jwt=JWTManager(app)
 
 
-
-## 챌린지
 ## 마이페이지
 ## 매점
+
+################## 챌린지 추천 코드################################
+# 1:등록금  2:해외여행  3:효도  4:청약  5:모의  6:교육
+R=np.array([[2.5,np.NaN,3.5,np.NaN,np.NaN,2.5],
+          [np.NaN,4.5,np.NaN,np.NaN,2,np.NaN],
+          [np.NaN,np.NaN,2.5,1.5,np.NaN,1],
+          [1.5,4.5,np.NaN,2,3,np.NaN],
+          [np.NaN,np.NaN,np.NaN,np.NaN,np.NaN,2.5]])
+
+num_user, num_items=R.shape
+
+K=2   # 행렬 분해 갯수
+np.random.seed(1)
+
+P=np.random.normal(scale=1./K, size=(num_user, K))
+Q=np.random.normal(scale=1./K, size=(num_items, K))
+
+# R>0 인 행 위치, 열 위치, 값을 non_zeros 리스트에 저장
+non_zeros = [(i,j,R[i,j]) for i in range(num_user) for j in range(num_items) if R[i,j]>0]
+
+def get_rmse(R,P,Q, non_zeros):
+  error=0
+  # 두개의 분해된 행렬 P와 Q.T의 내적으로 예측 R 행렬 생성
+  full_pred_matrix= np.dot(P,Q.T)
+  
+  # 실제 R 행렬에서 널이 아닌 값의 위치 인덱스 추출하여 실제 R 행렬과 예측 행렬의 RMSE추출
+  x_non_zero_ind=[non_zero[0] for non_zero in non_zeros]
+  y_non_zero_ind=[non_zero[1] for non_zero in non_zeros]
+  R_non_zeros = R[x_non_zero_ind, y_non_zero_ind]
+  full_pred_matrix_non_zeros = full_pred_matrix[x_non_zero_ind, y_non_zero_ind]
+  
+  mse=mean_squared_error(R_non_zeros, full_pred_matrix_non_zeros)
+  rmse= np.sqrt(mse)
+  
+  return rmse
+
+steps=900
+learning_rate=0.01
+r_lambda=0.01
+
+# SGD 기법으로 P와 Q 매트릭스를 계속 업데이트
+for step in range(steps):
+  
+  for i,j,r in non_zeros:
+    # 실제 값과 예측 값의 차이인 오류 값 구함
+    eij=r-np.dot(P[i,:],Q[j,:].T)
+    # Regularization을 반영한 SGD 업데이트 공식 적용
+    P[i,:]=P[i,:]+learning_rate*(eij * Q[j,:] - r_lambda*P[i,:])
+    Q[j,:]=Q[j,:]+learning_rate*(eij * P[i,:] - r_lambda*Q[j,:])
+    
+  rmse=get_rmse(R,P,Q, non_zeros)
+
+pred_matrix= np.dot(P,Q.T)
+
+
+#################################################################
+
+
+
+
+## 챌린지 추천
+@app.route('/challenge', methods=['GET','POST'])
+@jwt_required()
+def challenge():
+  user_id=get_jwt_identity()
+  
+  challenge_index=user_id-1   # userid와 challenge 추천 결과값의 인덱스 값의 차이가 1나기때문에 -1 해줘야한다.
+
+  real_matrix=R[challenge_index,:]    # 해당 유저의 행 뽑아냄
+  predict_matrix=np.round(pred_matrix, 3)
+  predict_matrix=pred_matrix[challenge_index,:]   # 해당 유저의 예측 행 뽑아냄
+
+  
+  max_challenge=0
+  max_index=0
+  # 가장 높은 선호도 추출
+  for i in range(len(real_matrix)):
+    if np.isnan(real_matrix[i]) and (predict_matrix[i]>max_challenge):
+      max_challenge=predict_matrix[i]
+      max_index=i+1
+      
+  return jsonify(chalid=max_index)
+  
+
+ 
+
+######################################################################################################
+
+
+###################################### Board API ######################################################
+# 게시판 저장
+@app.route('/board/save', methods=['GET','POST'])
+@jwt_required()
+def save_board():
+  data=request.get_json()
+  print(data)
+  chal_type=data['chal_type'] # 1:저축, 2:투자, 3:교육
+  title=data['title']
+  contents=data['contents']
+  user_id=get_jwt_identity()
+
+  connection= pymysql.connect(host=host, user=user, password=password, database=database)
+  conn=connection.cursor(pymysql.cursors.DictCursor)
+  query=f'insert into board (title,contents,userid,chaltype) values (%s,%s,%s,%s);'
+  conn.execute(query,(title,contents,user_id,chal_type))
+  connection.commit()
+  connection.close()
+  return jsonify(msg='게시글 저장이 완료되었습니다.')
+
+# 게시글 보여주기
+@app.route('/board', methods=['GET','POST'])
+@jwt_required()
+def board_home():
+  user_id=get_jwt_identity()
+  data=request.get_json()
+  board_type=data['chal_type']  # 1:저축, 2:투자, 3:교육
+
+  connection= pymysql.connect(host=host, user=user, password=password, database=database)
+  conn=connection.cursor(pymysql.cursors.DictCursor)
+  query=f'SELECT board.boardid,board.userid,board.title,board.contents,userlogin.name FROM board INNER JOIN userlogin ON board.userid = userlogin.userid where chaltype={board_type} ORDER BY boardid DESC;'
+  conn.execute(query)
+  result=conn.fetchall()
+
+  if result==None:
+    return jsonify(msg="게시물이 없습니다.")
+  else:
+    return jsonify({'result':result,'access_token':user_id})
+
+# 게시판 수정
+@app.route('/board/update' ,methods=['GET','POST'])
+@jwt_required()
+def board_update():
+  user_id=get_jwt_identity()
+  data=request.get_json()
+  boardid=data['boardid']
+  title=data['title']
+  contents=data['contents']
+  userid=data['userid']
+
+  # print(boardid,title,contents)
+  if userid==user_id:
+    connection= pymysql.connect(host=host, user=user, password=password, database=database)
+    conn=connection.cursor(pymysql.cursors.DictCursor)
+
+    query=f'update board set title="{title}", contents="{contents}" where boardid={boardid} and userid={user_id} ;'
+    conn.execute(query)
+    connection.commit()
+    connection.close()
+
+    return jsonify(msg="수정이 완료되었습니다")
+  else:
+    return jsonify(msg="수정 권한이 없습니다.")
+
+# 게시물 삭제
+@app.route('/board/delete', methods=['GET','POST'])
+@jwt_required()
+def board_delete():
+  data=request.get_json()
+  boardid=data['boardid']
+  user_id=get_jwt_identity()
+
+  connection= pymysql.connect(host=host, user=user, password=password, database=database)
+  conn=connection.cursor(pymysql.cursors.DictCursor)
+
+  query=f'delete from board where boardid={boardid} userid={user_id};'
+  conn.execute(query)
+  connection.commit()
+  connection.close()
+  return jsonify(msg="삭제가 완료되었습니다.")
+
+
+#######################################################################################################
+
 
 ######################################## UPDATE DB에 저장 #############################################
 
@@ -171,7 +344,7 @@ def dictionary():
 ## 퀴즈 던져주기
 @app.route('/home/quiz', methods=['GET','POST'])
 @jwt_required()
-def home_quiz():
+def home_quiz(): 
   cur_user=get_jwt_identity()
 
   connection=pymysql.connect(host=host, user=user, password=password, database=database)
@@ -189,7 +362,7 @@ def home_quiz():
     return jsonify(quiz_list)
   else:
     user_dict=user_dict.replace("[","(").replace("]",")")
-    print(user_dict)
+    # print(user_dict)
     query2=f'select * from dict where wordid not in {user_dict} limit 3;'
     conn.execute(query2)
     quiz_list=conn.fetchall()
